cb8cfc2a08f8f0bce4a87f78a307e3ad1cd52287
Abid Maqbool <abid.maqbool55@gmail.com>
2019-12-01 19:46:57 +0500

    Add cross platform functionality desktop & android.

--- android-javafx-helloworld/android-javafx-helloworldApp/src/main/java/com/android/javafx/helloworld/App.java
+++ android-javafx-helloworld/android-javafx-helloworldApp/src/main/java/com/android/javafx/helloworld/App.java
@@ -1,10 +1,11 @@
 package com.android.javafx.helloworld;
 
+import com.gluonhq.charm.down.common.PlatformFactory;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -12,10 +13,18 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        StackPane root = new StackPane();
+        VBox root = new VBox(10);
+        root.setPrefSize(335, 600);
 
-        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
-        Scene scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
+        Scene scene;
+        if (PlatformFactory.getPlatform().getName().equals(PlatformFactory.ANDROID)) {
+            Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+            scene = new Scene(root, visualBounds.getWidth(), visualBounds.getHeight());
+        } else {
+            scene = new Scene(root);
+
+            stage.setTitle("Android JavaFX Hello World App");
+        }
 
         stage.setScene(scene);
         stage.show();
