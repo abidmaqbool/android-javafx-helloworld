5d3d35758134b8999970398be15c1ff58ffb96be
Abid Maqbool <abid.maqbool55@gmail.com>
2019-12-01 19:47:41 +0500

    Add Hello Label and Exit Button.

--- android-javafx-helloworld/android-javafx-helloworldApp/src/main/java/com/android/javafx/helloworld/App.java
+++ android-javafx-helloworld/android-javafx-helloworldApp/src/main/java/com/android/javafx/helloworld/App.java
@@ -3,8 +3,11 @@ package com.android.javafx.helloworld;
 import com.gluonhq.charm.down.common.PlatformFactory;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -15,6 +18,12 @@ public class App extends Application {
     public void start(Stage stage) throws Exception {
         VBox root = new VBox(10);
         root.setPrefSize(335, 600);
+        root.setAlignment(Pos.CENTER);
+
+        Label lblHello = new Label("Android JavaFX Hello World App");
+        Button btnQuit = new Button("Quit");
+
+        root.getChildren().addAll(lblHello, btnQuit);
 
         Scene scene;
         if (PlatformFactory.getPlatform().getName().equals(PlatformFactory.ANDROID)) {
@@ -25,6 +34,10 @@ public class App extends Application {
 
             stage.setTitle("Android JavaFX Hello World App");
         }
+
+        btnQuit.setOnAction((event) -> {
+            System.exit(0);
+        });
 
         stage.setScene(scene);
         stage.show();
